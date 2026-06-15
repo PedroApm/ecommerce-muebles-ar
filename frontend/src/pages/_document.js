62d5553bdc7543eaf0780bad2733fbd9f7f008d7
PedroApm <146042938+PedroApm@users.noreply.github.com>
2026-06-14 23:58:38 -0500

    solucion carga boton "ver en mi espacio"

--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <script type="module" src="https://unpkg.com/@google/model-viewer@4.0.0/dist/model-viewer.min.js"></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
